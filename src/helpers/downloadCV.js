23d1eb12bcad48d78d35ba5068bf2f1ab1c719fd
Mariia <mcherkashina77@gmail.com>
2023-11-24 16:45:54 +0100

    Update downloadCV.js

--- src/helpers/downloadCV.js
+++ src/helpers/downloadCV.js
@@ -1,5 +1,6 @@
 export const handleDownloadCV = () => {
-  const pdfPath = "/public/1982264Сherkashyna_Mariia_FRONT-END_DEVELOPER.pdf";
+  const pdfPath =
+    "/path/to/your/file/1982264Сherkashyna_Mariia_FRONT-END_DEVELOPER.pdf";
   const link = document.createElement("a");
   link.href = pdfPath;
 
